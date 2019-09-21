b1c9209893cf3f27d1a4ed940d7c5463be1e91e8
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 20:31:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,13 +43,12 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  total = 0
-  new_cart = consolidate_cart(cart)
-  coupon_cart = apply_coupons(new_cart, coupons)
-  clearance_cart = apply_clearance(coupon_cartcart)
-  clearance_cart.each do |item, attribute_hash|
+  total = 0 
+  new_cart = consolidate_cart(cart) 
+  coupon_cart = apply_coupons(new_cart, coupons) 
+  clearance_cart = apply_clearance(coupon_cart) 
+  clearance_cart.each do |item, attribute_hash| 
     total += (attribute_hash[:price] * attribute_hash[:count])
-  end
-  total = (total * .9) if total >= 100
-  total
+  end 
+  total = (total * 0.9) if total > 100 total 
 end
