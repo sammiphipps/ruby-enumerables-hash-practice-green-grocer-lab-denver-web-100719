478918cc06dc62b76a3efc2b37e976c7d51d3bd7
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 19:51:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,7 +17,7 @@ def apply_coupons(cart, coupons)
   coupons.each{ |coupon|
     coupon.each{ |attribute, value|
       item = coupon[:item]
-      if cart[item]
+      if cart[item] && 
     }
   }
 end
