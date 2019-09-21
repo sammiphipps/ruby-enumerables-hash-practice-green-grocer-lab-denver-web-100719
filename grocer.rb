952ded069eb0db7b6aae878fa35f528571e5edbf
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 19:04:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,5 @@
 def consolidate_cart(cart)
-  new_cart = cart.reduce({}) do |memo, (key, value)|
-    if memo[key]
-      memo[key][:count] += 1
-    else
-      memo[key] = value 
-      memo[key][:count] = 1
-    end 
-    memo
-  end 
+  
 end
 
 def apply_coupons(cart, coupons)
