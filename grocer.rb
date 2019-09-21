6ace01f95f36d9130c07b5d3f7338a843b63116a
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 20:07:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,7 +20,7 @@ def apply_coupons(cart, coupons)
     
       if cart[name] && cart[name][:count] >= coupon[:num] 
         if cart["#{name} W/COUPON"] 
-          cart["#{name} W/COUPON"][:count] += 1 
+          cart["#{name} W/COUPON"][:count] += coupon[:num] 
         else 
           cart["#{name} W/COUPON"] = {:price => coupon[:cost]/coupon[:num],
           :clearance => cart[name][:clearance], :count => coupon[:num]} 
